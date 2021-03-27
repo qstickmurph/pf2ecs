df8d03e629efca13e59bd145142ea8ba3d2fab2a
Nragis <quinn_murphey@me.com>
2021-03-27 15:38:10 -0500

    Initialize character sheet

--- src/main/java/pf2ecs/model/CharacterSheet.java
+++ src/main/java/pf2ecs/model/CharacterSheet.java
@@ -1,39 +1,53 @@
 package pf2ecs.model;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Hashtable;
 
-import pf2ecs.model.Action;
 import pf2ecs.model.Attribute;
+import pf2ecs.model.Alignment;
+import pf2ecs.model.Scenario;
+import pf2ecs.model.SkillTraining;
+import pf2ecs.model.Size;
+import pf2ecs.model.Class;
+import pf2ecs.model.Ancestry
+import pf2ecs.model.Skill;
+import pf2ecs.model.Background;
 //import pf2ecs.model.Spell;
+import pf2ecs.model.Action;
+import pf2ecs.model.Inventory;
+import pf2ecs.model.Feat;
 
-/** The CharacterSheet combines all the information about a character in one place
- *
- * @author Quinn Murphey
- * @author David Gellhausen
- * @author Jim Richmond
- * @author Alexander Lomeli-Azoubel
- * @since 03/26/2021
- */
-public class CharacterSheet{
-
+/** The CharacterSheet class holds all the information about a character
+  *
+  * @author Quinn Murphey
+  * @author David Gellhausen
+  * @author Jim Richmond
+  * @author Alexander Lomeli-Azoubel
+  * @since 03/26/2021
+  */
+public class CharacterSheet {
+    
+    
+    
     /**
+     * Constructor Method default
      *
-     *
+     * @param name (String)
      */
     public CharacterSheet(){
-
-    }
-    
-   /*
-    *
-    *
-    */ 
-    public CharacterSheet(String filename){
         
     }
+    
 
-    public readJson(String filename, CharacterSheet characterSheet){
+    /**
+     * Constructor Method making a CharacterSheet from a json file.
+     *
+     * @param filename (String) This is the String used to create the character sheet.
+     */
+    public CharacterSheet(String filename){
 
     }
+
+    
 }
