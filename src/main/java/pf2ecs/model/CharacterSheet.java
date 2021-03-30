0f4d9f2f7461496ed88cc23ca9308c349fee9acf
Nragis <quinn_murphey@me.com>
2021-03-30 13:06:16 -0500

    Moved Inventory class function into CharacterSheet

--- src/main/java/pf2ecs/model/CharacterSheet.java
+++ src/main/java/pf2ecs/model/CharacterSheet.java
@@ -26,7 +26,6 @@ import pf2ecs.model.Skill;
 import pf2ecs.model.Background;
 //import pf2ecs.model.Spell;
 import pf2ecs.model.Action;
-import pf2ecs.model.Inventory;
 import pf2ecs.model.Feat;
 
 /** The CharacterSheet class holds all the information about a character
@@ -50,8 +49,29 @@ public class CharacterSheet {
     /** The Background object of the character */
     private Background background;
 
-    /** The character's inventory */
-    private Inventory inventory;
+	/** The items in the inventory */
+    private ArrayList<PfItem> items;
+    
+    /** The items equipped */
+    private ArrayList<PfItem> equipped;
+    
+    /** The items invested */
+    private ArrayList<PfItem> invested;
+    
+    /** The items readied */
+    private ArrayList<PfItem> readied;
+    
+    /** The amount of pp */
+    private int pp;
+    
+    /** The amount of gp */
+    private int gp;
+    
+    /** The amount of sp */
+    private int sp;
+    
+    /** The amount of cp */
+    private int cp;
 
     /** The actions the player has */
     private HashSet<Action> actions;
@@ -122,7 +142,16 @@ public class CharacterSheet {
         this.ancestry = new Ancestry();
         this.pfClass = new PfClass();
         this.background = new Background();
-        this.inventory = new Inventory();
+
+        this.items = new ArrayList<PfItem>();
+        this.equipped = new ArrayList<PfItem>();
+        this.invested = new ArrayList<PfItem>();
+        this.readied = new ArrayList<PfItem>();
+        this.pp = 0;
+        this.gp = 0;
+        this.sp = 0;
+        this.cp = 0;
+
         this.actions = new HashSet<>();
         this.feats = new HashSet<>();
         this.xp = 0;
@@ -272,21 +301,165 @@ public class CharacterSheet {
         this.background = background;
     }
 
-    /**
-     * Getter for this.inventory
-     * @return this.inventory (Inventory)
+    /** 
+     * Returns this.items
+     *  @return this.items (ArrayList<PfItem>)
      */
-    public Inventory getInventory(){
-        return this.inventory;
+    public ArrayList<PfItem> getItems(){
+        return this.items;
+    }
+
+    /** 
+     * Adds item to this.items
+     * @param item (String)
+     */
+    
+    public void addItem(PfItem pfItem){
+    	this.items.add(pfItem);
     }
     
-    /**
-     * Setter for this.inventory
-     * @param inventory (Inventory)
+    /** 
+     * Removes item from this.items
+     * @param item (String)
      */
-    public void setInventory(Inventory inventory){
-        this.inventory = inventory;
+    public void removeItem(PfItem pfItem){
+    	if(this.items.contains(pfItem)){
+    		this.items.remove(pfItem);
+    	}
     }
+
+    /** 
+     * Returns this.equipped
+     *  @return this.equipped (ArrayList<PfItem>)
+     */
+    public ArrayList<PfItem> getEquipped(){
+        return this.equipped;
+    }
+    
+    /** 
+     * Adds item to this.equipped and removes item from this.items
+     * @param item (String)
+     */
+    public void equipItem(PfItem pfItem){
+    	if(this.items.contains(pfItem)){
+    		this.equipped.add(pfItem);
+    		this.items.remove(pfItem);
+    	}
+    }
+
+    /** 
+     * Removes item from this.equipped and adds item to this.items
+     * @param item (String)
+     */
+    public void unequipItem(PfItem pfItem){
+    	if(this.equipped.contains(pfItem)){
+    		this.items.add(pfItem);
+    		this.equipped.remove(pfItem);
+    	}
+    }
+    
+    /** 
+     * Returns this.invested
+     *  @return this.invested (ArrayList<PfItem>)
+     */
+    public ArrayList<PfItem> getInvested(){
+        return this.invested;
+    }
+    
+    /** 
+     * Adds item to this.invested and this.equipped and removes item from this.items
+     * @param item (String)
+     */
+    public void investItem(PfItem pfItem){
+    	if(this.items.contains(pfItem)){
+    		this.invested.add(pfItem);
+    		this.equipped.add(pfItem);
+    		this.items.remove(pfItem);
+    	}
+    }
+    
+    /** 
+     * Removes item from this.invested and this.equipped and adds item to this.items
+     * @param item (String)
+     */
+    public void uninvestItem(PfItem pfItem){
+    	if(this.invested.contains(pfItem)){
+    		this.items.add(pfItem);
+    		this.invested.remove(pfItem);
+    		this.equipped.remove(pfItem);
+    	}
+    }
+    
+    /** 
+     * Returns this.readied
+     *  @return this.readied (ArrayList<PfItem>)
+     */
+    public ArrayList<PfItem> getReadied(){
+        return this.readied;
+    }
+    
+    /** 
+     * Adds item to this.readied and removes item from this.items
+     * @param item (String)
+     */
+    public void readyItem(PfItem pfItem){
+    	if(this.items.contains(pfItem)){
+    		this.readied.add(pfItem);
+    		this.items.remove(pfItem);
+    	}
+    }
+
+    /** 
+     * Removes item from this.readied and adds item to this.items
+     * @param item (String)
+     */
+    public void unreadyItem(PfItem pfItem){
+    	if(this.readied.contains(pfItem)){
+    		this.items.add(pfItem);
+    		this.readied.remove(pfItem);
+    	}
+    }
+    
+	/**
+	 * Getter for this.money.
+	 * @return Returns this.money
+	 */
+	public String getMoney(){
+		String money = Integer.toString(pp) + "pp " + Integer.toString(gp) + "gp " + Integer.toString(sp) + "sp " + Integer.toString(cp) + "cp";
+		return money;
+	}
+	
+	/**
+	 * Setter for this.pp.
+	 * @param pp (int) Sets this.pp to pp 
+	 */
+	public void setPp(int pp){
+		this.pp = pp;
+	}
+	
+	/**
+	 * Setter for this.gp.
+	 * @param gp (int) Sets this.gp to gp 
+	 */
+	public void setGp(int gp){
+		this.gp = gp;
+	}
+	
+	/**
+	 * Setter for this.sp.
+	 * @param sp (int) Sets this.sp to sp
+	 */
+	public void setSp(int sp){
+		this.sp = sp;
+	}
+	
+	/**
+	 * Setter for this.cp.
+	 * @param cp (int) Sets this.cp to cp 
+	 */
+	public void setCp(int cp){
+		this.cp = cp;
+	}
     
     /**
      * Getter for this.actions
@@ -754,7 +927,7 @@ public class CharacterSheet {
 
     public int getAcDexCap(){
         int min = Integer.MAX_VALUE;
-        for(PfItem item : this.inventory.getEquipped()){
+        for(PfItem item : this.equipped){
             if(item.getAcDexCap() < min){
                 min = item.getAcDexCap();
             }
