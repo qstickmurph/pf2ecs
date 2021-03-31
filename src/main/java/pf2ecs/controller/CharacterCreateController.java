334a4da24f127ca5055b1354d4c89ec0f9c67532
Nragis <quinn_murphey@me.com>
2021-03-31 05:32:38 -0500

    Created CharacterCreate fxml and initialized its controller class

---
+++ src/main/java/pf2ecs/controller/CharacterCreateController.java
@@ -0,0 +1,225 @@
+package pf2ecs.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class CharacterCreateController {
+    @FXML
+    private Tab ancestryTab;
+    @FXML
+    private ChoiceBox<?> ancestryChoiceBox;
+    @FXML
+    private Label ancestryDescription;
+    @FXML
+    private ChoiceBox<?> ancestryFeatChoiceBox;
+    @FXML
+    private Label ancestryFeatDescription;
+    @FXML
+    private Label ancestryHitPoints;
+    @FXML
+    private Label ancestrySize;
+    @FXML
+    private Label ancestrySpeed;
+    @FXML
+    private Label ancestryLanguages;
+    @FXML
+    private Label ancestryAbilityBoosts;
+    @FXML
+    private Label ancestryAbilityFlaws;
+    @FXML
+    private Label ancestryTraits;
+    @FXML
+    private Label ancestryFeatures;
+    @FXML
+    private Tab heritageBackgroundTab;
+    @FXML
+    private ChoiceBox<?> heritageChoiceBox;
+    @FXML
+    private Label heritageDescription;
+    @FXML
+    private ChoiceBox<?> backgroundChoiceBox;
+    @FXML
+    private Label backgroundDescription;
+    @FXML
+    private Label backgroundAbilityBoosts;
+    @FXML
+    private Label backgroundProficiencies;
+    @FXML
+    private Label backgroundFeat;
+    @FXML
+    private Label backgroundFeatDescription;
+    @FXML
+    private Tab classTab;
+    @FXML
+    private ChoiceBox<?> classChoiceBox;
+    @FXML
+    private Label classDescription;
+    @FXML
+    private Label classKeyAbility;
+    @FXML
+    private Label classHitPoints;
+    @FXML
+    private Tab classFeaturesTab;
+    @FXML
+    private GridPane classFeaturesGrid;
+    @FXML
+    private ChoiceBox<?> classLevel1Feat;
+    @FXML
+    private Label classLevel1FeatDescription;
+    @FXML
+    private Tab abilityScoresTab;
+    @FXML
+    private Label strength;
+    @FXML
+    private Label dexterity;
+    @FXML
+    private Label constitution;
+    @FXML
+    private Label intelligence;
+    @FXML
+    private Label wisdom;
+    @FXML
+    private Label charisma;
+    @FXML
+    private Tab skillsTab;
+    @FXML
+    private Tab spellsTab;
+    @FXML
+    private Tab equipmentTab;
+    @FXML
+    private Tab descriptionTab;
+    @FXML
+    private TextField charactedName;
+    @FXML
+    private TextField playerName;
+    @FXML
+    private TextField diety;
+    @FXML
+    private TextField gender;
+    @FXML
+    private TextField height;
+    @FXML
+    private TextField weight;
+    @FXML
+    private ChoiceBox<?> alignmentChoiceBox;
+    @FXML
+    private TextField hair;
+    @FXML
+    private TextField skin;
+    @FXML
+    private TextField eyes;
+    @FXML
+    private TextField age;
+    @FXML
+    private TextArea personalityTraits;
+    @FXML
+    private TextArea ideals;
+    @FXML
+    private TextArea bonds;
+    @FXML
+    private TextArea flaws;
+    @FXML
+    private TextArea appearanceContinued;
+    @FXML
+    private Tab notesTab;
+    @FXML
+    private Tab overviewTab;
+    @FXML
+    private Button createCharacterButton;
+    @FXML
+    private Label overviewStrength;
+    @FXML
+    private Label overviewDexterity;
+    @FXML
+    private Label overviewConstitution;
+    @FXML
+    private Label overviewIntelligence;
+    @FXML
+    private Label overviewWisdom;
+    @FXML
+    private Label overviewCharisma;
+    @FXML
+    private Label overviewFortitude;
+    @FXML
+    private Label overviewReflex;
+    @FXML
+    private Label overviewWill;
+    @FXML
+    private Label overviewPerception;
+    @FXML
+    private GridPane overviewSkillsGrid;
+    @FXML
+    private Label overviewHitPoints;
+    @FXML
+    private Label overviewWorth;
+    @FXML
+    private Label overviewClass;
+    @FXML
+    private Label overviewAncestry;
+    @FXML
+    private Label overviewClassFeat;
+    @FXML
+    private Label overviewBackground;
+    @FXML
+    private Label overviewAncestryFeat;
+    @FXML
+    private Label overviewHeritage;
+
+    @FXML
+    void abilityScoresTabSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ancestryTabSelection(ActionEvent event) {
+
+    }
+
+    @FXML
+    void classFeaturesTabSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void classTabSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void descriptionTabSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void heritageBackgroundTabSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void notesTabSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void overviewTabSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void skillsTabSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void spellsTabSelected(ActionEvent event) {
+
+    }
+
+}
+
